e7bee9ca07396a6107a546f2c2ec44ad82add193
Alistair Crawford <alistair@wobdev.co.uk>
2021-12-17 06:16:41 +0000

    small change

--- timelapse.py
+++ timelapse.py
@@ -48,7 +48,7 @@ def capture_images(length_in_seconds,interval_in_seconds, rotation):
     with picamera.PiCamera() as camera:
         if(__night_mode__):
             print("Trying to set exposure to night")
-            camera.exposure_mode = 'off'
+            camera.exposure_mode = 'night'
             camera.iso = 800
         camera.start_preview()
         print(camera.exposure_mode)
